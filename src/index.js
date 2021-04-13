9aeabdf895cbe48f3e31eefc9915cab5f8875aa2
Tý Quậy <yamibakura2016gmail.com>
2021-04-13 22:38:49 +0700

    123

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
-const handlebars = require('express-handlebars');
-const morgan = require('morgan');
-const route = require('./routes');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const route = require('./routes')
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan ('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Template engine
-app.engine(
-  'hbs',
-  handlebars({
-    extname: '.hbs',
-  }),
-);
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}))
 
-                                      app.set('view engine', 'hbs');
-                                                      app.set('views', path.join(__dirname, 'resource\\views'));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'resource\\views'))
 
 //Route init
-route(app);
+route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
